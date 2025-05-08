95be870307b7a06621022fd9e1067ed0e69a2949
seanivan69 <ostriashan@gmail.com>
2025-05-09 00:03:25 +0800

    updated workflows

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('config.json');
 const mysql = require('mysql2/promise');
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
 
@@ -18,31 +18,11 @@ async function initialize() {
     // init models and add them to the exported db object
     db.Account = require('../accounts/account.model')(sequelize);
     db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
-    db.Employee = require('../employees/employee.model')(sequelize, DataTypes);
-    db.Department = require('../departments/department.model')(sequelize, DataTypes);
-    db.Workflow = require('../workflows/workflow.model')(sequelize, DataTypes);
-    db.Request = require('../requests/request.model')(sequelize, DataTypes);
 
     // define relationships
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
 
-    db.Account.hasOne(db.Employee, { foreignKey: 'accountId', as: 'employee' });
-    db.Employee.belongsTo(db.Account, { foreignKey: 'accountId' });
-
-    db.Department.hasMany(db.Employee, { foreignKey: 'departmentId', as: 'employees' });
-    db.Employee.belongsTo(db.Department, { foreignKey: 'departmentId' });
-
-    db.Employee.hasMany(db.Workflow, { foreignKey: 'employeeId' });
-    db.Workflow.belongsTo(db.Employee, { foreignKey: 'employeeId' });
-
-    db.Employee.hasMany(db.Request, { foreignKey: 'employeeId' });
-    db.Request.belongsTo(db.Employee, { foreignKey: 'employeeId' });
-
     // sync all models with database
     await sequelize.sync({ alter: true });
-
-    // expose sequelize instance to be used throughout the app
-    db.sequelize = sequelize;
-    db.Sequelize = Sequelize;
 }
--- departments/index.js
+++ departments/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../_helpers/db');
-const authorize = require('../_middleware/authorize');
-const Role = require('../_helpers/role');
+const db = require('./_helpers/db');
+const authorize = require('./_middleware/authorize');
+const Role = require('./_helpers/role');
 
 router.post('/', authorize(Role.Admin), create);
-router.get('/', authorize(), getAll);
-router.get('/:id', authorize(), getById);
+router.get('/', authorize, getAll);
+router.get('/:id', authorize, getById);
 router.put('/:id', authorize(Role.Admin), update);
 router.delete('/:id', authorize(Role.Admin), _delete);
 
@@ -14,9 +14,7 @@ async function create(req, res, next) {
     try {
         const department = await db.Department.create(req.body);
         res.status(201).json(department);
-    } catch (err) {
-        next(err);
-    }
+    } catch (err) { next(err); }
 }
 
 async function getAll(req, res, next) {
@@ -28,21 +26,16 @@ async function getAll(req, res, next) {
             ...d.toJSON(),
             employeeCount: d.Employees.length
         })));
-    } catch (err) {
-        next(err);
-    }
+    } catch (err) { next(err); }
 }
-
 async function getById(req, res, next) {
     try {
         const department = await db.Department.findByPk(req.params.id, {
-            include: [{ model: db.Employee }]
+            include: { model: db.Employee, attributes: ['id'] }
         });
         if (!department) throw new Error('Department not found');
-        res.json(department);
-    } catch (err) {
-        next(err);
-    }
+        res.json({ ...department.toJSON(), employeeCount: department.Employees.length });
+    } catch (err) { next(err); }
 }
 
 async function update(req, res, next) {
@@ -51,9 +44,7 @@ async function update(req, res, next) {
         if (!department) throw new Error('Department not found');
         await department.update(req.body);
         res.json(department);
-    } catch (err) {
-        next(err);
-    }
+    } catch (err) { next(err); }
 }
 
 async function _delete(req, res, next) {
@@ -62,9 +53,7 @@ async function _delete(req, res, next) {
         if (!department) throw new Error('Department not found');
         await department.destroy();
         res.json({ message: 'Department deleted' });
-    } catch (err) {
-        next(err);
-    }
+    } catch (err) { next(err); }
 }
 
 module.exports = router;
